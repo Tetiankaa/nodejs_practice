e3d49cb4bbe30c0f5aa1c5b42e9290104798a93c
Tetiankaa <tanyakorsun0208@gmail.com>
2024-04-29 18:08:34 +0200

    creating simple web server with redirection logic and with dynamically displaying pages

---
+++ server.js
@@ -0,0 +1,106 @@
+const EventEmitter = require("node:events");
+const emitter = new EventEmitter();
+
+const http = require('node:http');
+const PORT = process.env.PORT || 3500;
+const path = require("node:path");
+const fs = require("node:fs");
+const fsPromises = require("node:fs/promises");
+
+// const server = http.createServer((req, res)=>{
+//     let pathToFile;
+//
+//     if (req.url === '/'){
+//         res.statusCode = 200;
+//         res.setHeader('Content-Type','text/html');
+//         pathToFile = path.join(__dirname,'views','index.html');
+//         fs.readFile(pathToFile,'utf-8',(err, data)=>{
+//             res.end(data);
+//         })
+//     }
+// })
+
+const serveFile = async (filePath, contentType, response) =>{
+    try {
+        const rawData = await fsPromises.readFile(filePath,!contentType.includes('image') ? 'utf-8' : '');
+        const data = contentType === 'application/json' ? JSON.parse(rawData) : rawData;
+
+        response.writeHead(
+            filePath.includes('404.html') ? 404 :200,
+            {'Content-Type':contentType}
+        );
+        response.end(
+            contentType === 'application/json' ? JSON.stringify(data) : data
+        )
+    }catch (e) {
+        console.error(e.message);
+        response.statusCode = 500;
+        response.end();
+    }
+}
+
+const server = http.createServer((req, res)=>{
+
+    console.log(req.url)
+    console.log('slice: ' + req.url.slice(-1))
+    console.log('ext: ' + path.extname(req.url))
+
+    const extension = path.extname(req.url);
+    let contentType;
+
+    switch (extension){
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.txt':
+            contentType = 'txt/plain';
+            break;
+        default:
+            contentType = 'text/html';
+    }
+
+    let filePath =
+        contentType === 'text/html' && req.url === '/'
+            ? path.join(__dirname, 'views','index.html')
+            : contentType === 'text/html' && req.url.slice(-1) === '/'
+                ? path.join(__dirname,'views',req.url,'index.html')
+                : contentType === 'text/html'
+                    ? path.join(__dirname, 'views', req.url)
+                    : path.join(__dirname, req.url)
+
+    if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
+
+    const fileExists = fs.existsSync(filePath);
+    console.log(path.parse(filePath).base)
+    if (fileExists){
+           void serveFile(filePath,contentType,res);
+    }else {
+       switch (path.parse(filePath).base) {
+           case 'old-page.html':
+               res.writeHead(301,{'Location':'/new-page.html'});
+               res.end();
+               break;
+           case 'www-page.html':
+               res.writeHead(301,{'Location':'/'});
+               res.end();
+               break;
+           default:
+              void serveFile(path.join(__dirname,'views','404.html'), 'text/html', res);
+       }
+    }
+})
+server.listen(PORT,()=>{
+    console.log(`Server running at http://localhost:${PORT}`);
+})
